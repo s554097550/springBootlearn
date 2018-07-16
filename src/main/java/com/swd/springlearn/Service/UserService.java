44d5f99d4f4c7762173b1b7b6d70b14a3f837c86
s554097550 <554097550@qq.com>
2018-07-16 14:40:23 +0800

    Revert "Revert "新增thymeleaf学习""
    This reverts commit 8dce02e25af5d50703adeff52e0e0b79c9370c8c.

---
+++ src/main/java/com/swd/springlearn/Service/UserService.java
@@ -0,0 +1,34 @@
+package com.swd.springlearn.Service;
+
+import com.swd.springlearn.Dao.UserDao;
+import com.swd.springlearn.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserDao userDao;
+
+    public List<User> findAllUser(){
+        return userDao.findAllUser();
+    }
+
+    public User findUserById(Integer id){
+        return userDao.findUserById(id);
+    }
+
+    public int addUser(User user){
+        return userDao.addUser(user);
+    }
+
+    public int delUserById(Integer id){
+        return userDao.delUserById(id);
+    }
+
+    public int editUser(Integer id, User user){
+        return  userDao.editUser(id,user);
+    }
+}
---
+++ src/main/java/com/swd/springlearn/entity/Author.java
@@ -0,0 +1,40 @@
+package com.swd.springlearn.entity;
+
+public class Author {
+    private String name;
+    private String englishName;
+    private int age;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
