1722e6c9151a00f3a7ce2817e07e51f41561f24a
swide <554097550@qq.com>
2018-09-05 20:58:01 +0800

    学习使用schedule

--- src/main/java/com/swd/springlearn/SpringlearnApplication.java
+++ src/main/java/com/swd/springlearn/SpringlearnApplication.java
@@ -5,15 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.scheduler.Scheduler;
 
 import java.util.Arrays;
 
 /**
  * @author RDKJ_PC
  */
-@RestController
+@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class SpringlearnApplication {
 
@@ -21,5 +28,11 @@ public class SpringlearnApplication {
         SpringApplication.run(SpringlearnApplication.class, args);
     }
 
+    @Bean
+    public TaskScheduler taskScheduler(){
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(10);
+        return threadPoolTaskScheduler;
+    }
 }
 
